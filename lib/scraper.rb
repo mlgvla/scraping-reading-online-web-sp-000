f657a7ebeb064e2e0c86509f976bd69463503778
Monica Gerard <mlgerardvla@gmail.com>
2020-03-26 00:55:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,4 +5,10 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
-doc.css(".headline-26OIBN").text
+
+
+courses = doc.css("#cd016a4c-d090-5b88-9be3-63e6d9187edf .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
