045aa235761745dc666e3318cb2d3857e7cdac24
Monica Gerard <mlgerardvla@gmail.com>
2020-03-26 01:19:08 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("https://flatironschool.com/our-courses/")
+
+doc = Nokogiri::HTML(html)
+
+
+
+doc.css("#cd016a4c-d090-5b88-9be3-63e6d9187edf .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").text.strip
